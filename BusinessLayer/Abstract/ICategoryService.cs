eafea1fd870ed13b4a8bd85b6b4b6eed959fb4da
Baran <byasarn@gmail.com>
2023-01-17 14:02:25 +0300

    Add project files.

---
+++ BusinessLayer/Abstract/ICategoryService.cs
@@ -0,0 +1,17 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Abstract
+{
+    public interface ICategoryService
+    {
+        public void CategoryAdd(Category category);
+        public void CategoryRemove(Category category);
+        public void CategoryUpdate(Category category);
+
+    }
+}
---
+++ BusinessLayer/Concrete/CategoryManager.cs
@@ -0,0 +1,36 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class CategoryManager : ICategoryService
+    {
+
+        EfCategoryRepository _efCategoryRepository;
+
+        public CategoryManager()
+        {
+            _efCategoryRepository = new EfCategoryRepository();
+        }
+        public void CategoryAdd(Category category)
+        {
+        _efCategoryRepository.Insert(category);
+        }
+
+        public void CategoryRemove(Category category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CategoryUpdate(Category category)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
