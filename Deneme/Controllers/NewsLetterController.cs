46252d5fe257ea626940677cbdf44f419402c0fe
Baran <byasarn@gmail.com>
2023-01-20 10:06:09 +0300

    Mail Bulteni Abone

---
+++ Deneme/Controllers/NewsLetterController.cs
@@ -0,0 +1,28 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Deneme.Controllers
+{
+    public class NewsLetterController : Controller
+    {
+
+        NewsLetterManager nm = new NewsLetterManager(new EfNewsLetterRepository());
+        
+        [HttpGet]
+        public PartialViewResult SubscribeMail()
+        {
+            return PartialView();
+        }
+
+        [HttpPost]
+        public IActionResult SubscribeMail(NewsLetter p)
+        {
+            p.MailStatus = true;
+            nm.AddNewsLetter(p);
+            Response.Redirect("/BlogController1/Index");
+            return PartialView();
+        }
+    }
+}
