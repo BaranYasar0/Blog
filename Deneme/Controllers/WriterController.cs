7164c8f4af78769e521935d590cd1251df44dd7b
Baran <byasarn@gmail.com>
2023-01-20 15:39:17 +0300

    Giriş ve yetkilendirme

--- Deneme/Controllers/WriterController.cs
+++ Deneme/Controllers/WriterController.cs
@@ -1,12 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Deneme.Controllers
 {
 	public class WriterController : Controller
 	{
+		[AllowAnonymous]
 		public IActionResult Index()
 		{
 			return View();
 		}
+
+		public IActionResult WriterProfile()
+		{
+			return View();
+		}
+
 	}
 }
--- Deneme/Program.cs
+++ Deneme/Program.cs
@@ -1,8 +1,19 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Authorization;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
+builder.Services.AddSession();
+builder.Services.AddMvc(config =>
+{
+    var policy=new AuthorizationPolicyBuilder()
+              .RequireAuthenticatedUser()
+              .Build();
+    config.Filters.Add(new AuthorizeFilter(policy));
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -12,6 +23,8 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+app.UseSession();
 
 app.UseStatusCodePagesWithReExecute("/ErrorPage/Error1","?code={0}");
 
