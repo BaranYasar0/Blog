f657b0aa696bc3f88c58b250d86d797573118df9
Baran <byasarn@gmail.com>
2023-01-21 16:08:53 +0300

    Kategorileri dropdown ile seçtirme

---
+++ BusinessLayer/ValidationRules/BlogValidator.cs
@@ -0,0 +1,22 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class BlogValidator:AbstractValidator<Blog>
+    {
+        public BlogValidator()
+        {
+            RuleFor(x => x.BlogTitle).NotEmpty().WithMessage("Başlık Boş Geçilemez.");
+            RuleFor(x => x.BlogContent).NotEmpty().WithMessage("İçerik Boş Geçilemez.");
+            RuleFor(x => x.BlogImage).NotEmpty().WithMessage("Görsel Boş Geçilemez.");
+            RuleFor(x => x.BlogTitle).MaximumLength(150).WithMessage("Max 150 karakter girebilirsiniz.").
+                MinimumLength(3).WithMessage("Minimum 3 Karakter giriniz."); ;
+        }
+    }
+}
--- Deneme/Controllers/BlogController1.cs
+++ Deneme/Controllers/BlogController1.cs
@@ -1,8 +1,11 @@
 ﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
 using DataAccessLayer.EntityFramework;
 using EntityLayer.Concrete;
+using FluentValidation.Results;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Deneme.Controllers
 {
@@ -34,12 +37,30 @@ namespace Deneme.Controllers
         [HttpGet]
         public IActionResult BlogAdd()
         {
+            CategoryManager cm = new CategoryManager(new EfCategoryRepository());
+            List<SelectListItem> values = (from x in cm.GetAll()
+                                           select new SelectListItem
+                                           {
+                                               Text = x.CategoryName,
+                                               Value = x.CategoryID.ToString()
+                                           }).ToList();
+            ViewBag.cv=values;
             return View();
         }
         
         [HttpPost]
         public IActionResult BlogAdd(Blog p)
         {
+            BlogValidator bv = new BlogValidator();
+            ValidationResult results = bv.Validate(p);
+            if (results.IsValid)
+            {
+                p.BlogStatus = true;
+                p.BlogCreateDate = DateTime.Parse(DateTime.Now.ToShortDateString());
+                p.WriterID = 4;
+                blogManager.TAdd(p);
+                return RedirectToAction("BlogListByWriter","BlogController1");
+            }
             return View();
         }
     }
