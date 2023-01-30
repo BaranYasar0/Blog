28c182168cb751e1d3c2343f519ad852dd7b7e71
Baran <byasarn@gmail.com>
2023-01-30 17:07:44 +0300

    Identity kurulumu ve ajax

--- Deneme/Areas/Admin/Controllers/WriterController.cs
+++ Deneme/Areas/Admin/Controllers/WriterController.cs
@@ -13,15 +13,46 @@ namespace Deneme.Areas.Admin.Controllers
             return View();
         }
 
+        public IActionResult GetWriterById(int writerid)
+        {
+            var findWriter = writers.FirstOrDefault(x => x.Id == writerid);
+            var jsonWriter= JsonConvert.SerializeObject(findWriter);
+            return Json(jsonWriter);
+        }
+
+        [HttpPost]
+        public IActionResult AddWriter(WriterClass w)
+        {
+            writers.Add(w);
+            var jsonWriters = JsonConvert.SerializeObject(w);
+            return Json(jsonWriters);
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteWriter(int id)
+        {
+            var writer = writers.FirstOrDefault(x => x.Id == id);
+            writers.Remove(writer);
+            return Json(writer);
+        }
+
+        [HttpPut]
+        public IActionResult UpdateWriter(WriterClass w)
+        {
+            var writer = writers.FirstOrDefault(x => x.Id == w.Id);
+            writer.Name = w.Name;
+            var json = JsonConvert.SerializeObject(w);
+            return Json(json);
+        }
+
         public IActionResult WriterList()
         {
-            List<WriterClass> writers = new List<WriterClass> {
-        new WriterClass{Id=1,Name="brn"},
-        new WriterClass{Id=2,Name="brn2"},
-        new WriterClass{Id=3,Name="brn3"} };
-
             var jsonWriters = JsonConvert.SerializeObject(writers);
             return Json(jsonWriters);
         }
+        public static List<WriterClass> writers = new List<WriterClass> {
+        new WriterClass{Id=1,Name="brn"},
+        new WriterClass{Id=2,Name="brn2"},
+        new WriterClass{Id=3,Name="brn3"} };
     }
 }
---
+++ Deneme/Controllers/EmployeeTestController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Deneme.Controllers
+{
+    public class EmployeeTestController : Controller
+    {
+       
+    }
+}
